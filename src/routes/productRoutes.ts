332039d2a7a6d56b4b4d798103a1bb5b67114785
Alan <sachero10@gmail.com>
2026-02-12 19:57:09 -0300

    Ruta de actualizacion de Producto de PATCH a PUT

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -14,7 +14,7 @@ const router = Router();
 router.post("/", createProduct); // POST /api/products
 router.get("/", getProducts); // GET /api/products
 router.patch("/:id", updateProduct); // PATCH /api/products/ID_DEL_PRODUCTO. Actualiza datos generales del producto (nombre, descripción, marca, categoría)
-router.patch("/variants/:id/stock", updateStock); // PATCH /api/products/variants/:id/stock - Actualizar stock de una variante específica
+router.put("/variants/:id/stock", updateStock); // PUT /api/products/variants/:id/stock - Actualizar stock de una variante específica
 router.delete("/:id", deleteProduct); // DELETE /api/products/ID_DEL_PRODUCTO
 router.patch("/:id/restore", restoreProduct); // PATCH /api/products/ID_DEL_PRODUCTO/restore. Restaura un producto eliminado lógicamente
 
